ca91c0fc6e3c53484dedcc7d3144e41b57352097
Pradip Ram <pradipramnawa@gmail.com>
2025-07-05 11:45:08 +0530

    center the text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,27 +4,32 @@ import { Typography } from "@mui/material";
 import "../../assets/styles/footer.scss";
 
 interface FooterProps {
-    websiteName: string;
-    bgColor?: string;
-    className?: string;
-    style?: React.CSSProperties;
+  websiteName: string;
+  bgColor?: string;
+  className?: string;
+  style?: React.CSSProperties;
 }
 
-const Footer: React.FC<FooterProps> = ({ websiteName, bgColor,className,style }) => {
-    const currentYear = new Date().getFullYear();
+const Footer: React.FC<FooterProps> = ({
+  websiteName,
+  bgColor,
+  className,
+  style,
+}) => {
+  const currentYear = new Date().getFullYear();
 
-    const footerStyle: React.CSSProperties = {
-        backgroundColor: bgColor,
-        ...style
-    };
+  const footerStyle: React.CSSProperties = {
+    backgroundColor: bgColor,
+    ...style,
+  };
 
-    return (
-        <footer className={`footer ${className}`} style={footerStyle}>
-            <Typography>
-                &copy; {currentYear} {websiteName}. All rights reserved.
-            </Typography>
-        </footer>
-    );
+  return (
+    <footer className={`footer ${className}`} style={footerStyle}>
+      <Typography style={{ textAlign: "center" }}>
+        &copy; {currentYear} {websiteName}. All rights reserved.
+      </Typography>
+    </footer>
+  );
 };
 
 export default Footer;
