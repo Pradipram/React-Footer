1ab10d64eca6ae1e69cff40b5c5ed89bf584deea
Pradip Ram <pradipramnawa@gmail.com>
2025-07-05 14:44:46 +0530

    centering text vertically and horizontally

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Typography } from "@mui/material";
 
 import "../../assets/styles/footer.css";
@@ -6,31 +6,27 @@ import "../../assets/styles/footer.css";
 interface FooterProps {
   websiteName: string;
   bgColor?: string;
+  className?: string;
   style?: React.CSSProperties;
 }
 
-const Footer: React.FC<FooterProps> = ({ websiteName, bgColor, style }) => {
-  const [year, setYear] = useState<number | null>(null);
-
-  useEffect(() => {
-    setYear(new Date().getFullYear());
-  }, []);
-
-  const cssBgColor = bgColor || "#282c34";
+const Footer: React.FC<FooterProps> = ({
+  websiteName,
+  bgColor,
+  className,
+  style,
+}) => {
+  const currentYear = new Date().getFullYear();
 
   const footerStyle: React.CSSProperties = {
-    backgroundColor: cssBgColor,
-    color: "#ffffff",
-    padding: "10px 20px",
-    textAlign: "center",
-    minHeight: "60px",
+    backgroundColor: bgColor,
     ...style,
   };
 
   return (
-    <div className="footer" style={footerStyle}>
+    <div className={`footer ${className}`} style={footerStyle}>
       <Typography variant="body2">
-        &copy; {year} {websiteName}. All rights reserved.
+        &copy; {currentYear} {websiteName}. All rights reserved.
       </Typography>
     </div>
   );
